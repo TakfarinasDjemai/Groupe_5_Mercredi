9b1d5d69e8acaa86e3771de0d8001e7c9c5afd22
Takfarinas <Takfarinas@LAPTOP-MOEGDCGP>
2022-04-09 18:46:04 -0400

    Add project files.

---
+++ Groupe_5_Mercredi/Cours.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Groupe_5_Mercredi
+{
+    internal class Cours
+    {
+    }
+}
---
+++ Groupe_5_Mercredi/Test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Groupe_5_Mercredi
+{
+    internal class Test
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
