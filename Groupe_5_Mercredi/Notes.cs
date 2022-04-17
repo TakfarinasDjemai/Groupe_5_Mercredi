3fcc1cdfe2655960da9454bc5146262b17f003c4
marouaneAbdeL <102846276+marouaneAbdeL@users.noreply.github.com>
2022-04-17 04:34:41 -0400

    Update Notes.cs

--- Groupe_5_Mercredi/Notes.cs
+++ Groupe_5_Mercredi/Notes.cs
@@ -1,16 +1,41 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Groupe_5_Mercredi
+namespace PojetFinalAssurancelogiciel
 {
-    internal class Notes
+    internal class Notes : Etudiant
     {
-        //Chaque étudiant est identifié par:
-        //-Numéro d'étudiant
-        //-Numéro du cours
-        //-Note
+        //public int NumeroEtudiant { get; set; }
+        //public int NumeroCour { get; set; }
+        public double Note { get; set; }
+
+
+         public Notes(int numeroEtudiant, string nom, string prenom,int numeroCour, int code, string titre, double note) :
+            base(numeroEtudiant,nom,prenom,numeroCour,code,titre)
+        {
+            Note = note;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null) return false;
+            if (!(obj is Cube)) return false;
+            Notes other = (Notes)obj; //OPERATION DE CONVERSION
+            return base.Equals(obj) &&
+                Note == other.Note;
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+        //6- Doit-on redéfinir la méthode ToString?
+        public override string ToString()
+        {
+            return base.ToString() + "\n" +
+                $"Note : {Note}";
+        }
     }
 }
