327dc65e3d04d00ad1c89077f7d8bb64f4a9125f
marouaneAbdeL <102846276+marouaneAbdeL@users.noreply.github.com>
2022-04-17 04:32:52 -0400

    Update Cours.cs

--- Groupe_5_Mercredi/Cours.cs
+++ Groupe_5_Mercredi/Cours.cs
@@ -1,17 +1,64 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Groupe_5_Mercredi
+namespace PojetFinalAssurancelogiciel
 {
-    internal class Cours
+    internal class Cours : Etudiant
     {
-        //Chaque étudiant est identifié par:
-        //-Numéro du cours
-        //-Code
-        //-Titre
+        public int NumeroCour { get; private set ;} 
+        public int Code { get; set} 
+        public string Titre { get; set} 
+
+        public Cours(int numeroEtudiant, string nom, string prenom,int numeroCour, int code, string titre) :
+            base(numeroEtudiant,nom,prenom)
+
+        {
+            NumeroCour = numeroCour;
+            Code = code;
+            Titre = titre;
+
+        }
+       
+
+        //Redéfinir la méthode Equals.
+        public override bool Equals(object obj)
+        {
+            if (obj == null) return false;
+            if (!(obj is Cours)) return false;
+            Cours other = (Cours)obj; //OPERATION DE CONVERSION
+            return base.Equals(obj) &&
+                NumeroCour == other.NumeroCour &&
+                Code == other.Code &&
+                Titre == other.Titre;
+
+
+        }
+        
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        //redifinir la methode TosTRING
+        public override string ToString()
+        {
+            return base.ToString() + "\n" +                
+            $"Cour titre : {Titre} " + "\n" +
+                   $" Cour code: {Code}";
+        }
+
+
+
+
+
+
+
+
+
+
 
     }
 }
