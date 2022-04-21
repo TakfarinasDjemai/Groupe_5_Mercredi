c35a0face118e6879525f8fb698c5075e4da95b6
MohamedAboubaker <103153202+MohamedAboubaker@users.noreply.github.com>
2022-04-21 12:19:20 -0400

    Update Test.cs

--- Groupe_5_Mercredi/Test.cs
+++ Groupe_5_Mercredi/Test.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +10,41 @@ namespace Groupe_5_Mercredi
     {
         static void Main(string[] args)
         {
+            Etudiant e1 = new Etudiant(2699880, "Aboubaker", "Mohamed");
+            Etudiant e2 = new Etudiant(2832043, "Abdoul", "Karim");
+
+            Console.WriteLine(e1.ToString());
+            Console.WriteLine("******************************");
+            Console.WriteLine(e2.ToString());
+            Console.WriteLine("******************************");
+            Console.WriteLine(e2 == e1);
+            Console.WriteLine("******************************");
+
+            Cours c1 = new Cours(20304, "Math");
+            Cours c2 = new Cours(32942, "English");
+
+            Console.WriteLine(c1.ToString());
+            Console.WriteLine("******************************");
+            Console.WriteLine(c2.ToString());
+            Console.WriteLine("******************************");
+            Console.WriteLine(c2 == c1);
+            Console.WriteLine("******************************");
+
+
+            Notes n1 = new Notes(2699880, 20304, 32);
+            Notes n2 = new Notes(2832043, 32942, 24);
+
+            n1.AjouterCours(c1);
+            n1.AjouterEtudiant(e1);
+            n2.AjouterCours(c2);
+            n2.AjouterEtudiant(e2);
+
+            Console.WriteLine(n1.ToString());
+            Console.WriteLine("******************************");
+            Console.WriteLine(n2.ToString());
+            Console.WriteLine("******************************");
+            Console.WriteLine(n2 == n1);
+
         }
     }
 }
